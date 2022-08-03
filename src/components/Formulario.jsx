d1865f14370581ece019f1750ff1e59e49e10370
Juan Menichetti <jpmenichetti13@gmail.com>
2022-08-03 19:23:29 -0300

    se agrego las keys y props para la listaTareas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -34,7 +34,7 @@ const Formulario = () => {
           <Form.Control
             type="text"
             placeholder="Ingrese una tarea"
-            onChange={(e) => setTarea(e.target.value.trim())}
+            onChange={(e) => setTarea(e.target.value.trimStart())}
             value={tarea}
           />
           <Button variant="primary" type="submit" className="ms-1">
--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import {ListGroup} from 'react-bootstrap';
+import { ListGroup } from "react-bootstrap";
 import ItemTarea from "./ItemTarea";
 
-const ListaTareas = () => {
+const ListaTareas = (props) => {
   return (
     <div>
       <ListGroup>
-        <ItemTarea></ItemTarea>
+        {props.arregloTareas.map((item, posicion) => (
+          <ItemTarea key={posicion} tareaCargada={item}></ItemTarea>
+        ))}
       </ListGroup>
     </div>
   );
